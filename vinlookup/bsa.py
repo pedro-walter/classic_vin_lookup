a410537045067da573029b760c21ec1c7c0bad94
Vincent Davis <vincent@heteroskedastic.com>
2016-01-29 13:39:56 -0700

    add example in

--- vinlookup/bsa.py
+++ vinlookup/bsa.py
@@ -1,1 +1,13 @@
 # BSA
+"""
+Examples below, always return list becuase of possible multiple matches. See code for triumph.
+Examples:
+    input:
+        UYD-105
+    output:
+        Major Model: Bantam Sub Model: D1 Engine Start:  UYD-101 Engine # end:  Frame # start:  Frame # end:  Frame Type:  Notes and options:  Gears:  Electric: Wipac
+    input:
+        ZC10-101
+    output:
+        Major Model: C Range Sub Model: C11 Engine Start:  ZC11-101 Engine # end:  Frame # start: ZC10-101 Frame # end:  Frame Type: Rigid Notes and options:  Gears:  Electric:
+"""
