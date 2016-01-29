d44506cf3e7186076e0307aa58ab001bf19fd9d0
Vincent Davis <vincent@heteroskedastic.com>
2016-01-29 12:39:32 -0700

    add BSA notes

--- vinlookup/tests/test_triumph.py
+++ vinlookup/tests/test_triumph.py
@@ -4,9 +4,15 @@ from vinlookup import triumph as t
 
 class test_tri_decode(TestCase):
     def test_years_13_23(self):
-        vins = (('194878', ['Frame#, JUNIOR 2STROKE Model 1913']),)
+        vins = (('194878', ['Frame#, JUNIOR 2STROKE Model 1913']),
+                ('7899', ['Engine#, JUNIOR 2STROKE Model 1922']))
         for v, r in vins:
             self.assertEqual(t.decode(v), r)
+
+     def test_pre_unit_1950_1962(self):
+            vins = (('du35987', ['Unit 650cc  Model year 1966']),)
+            for v, r in vins:
+                self.assertEqual(t.decode(v), r)
 
     def test_unit650(self):
         vins = (('du35987', ['Unit 650cc  Model year 1966']),)
